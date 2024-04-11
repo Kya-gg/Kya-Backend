98aa1c4c349cb378ce385d539278d9563823c68a
ElroyKR <elroykr@gmail.com>
2024-04-11 22:55:09 +0900

    Initialize Commit

---
+++ src/main/kotlin/gg/kya/KyaApplication.kt
@@ -0,0 +1,11 @@
+package gg.kya
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KyaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KyaApplication>(*args)
+}
---
+++ src/test/kotlin/gg/kya/KyaApplicationTests.kt
@@ -0,0 +1,13 @@
+package gg.kya
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KyaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
