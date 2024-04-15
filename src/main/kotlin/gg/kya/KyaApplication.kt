5623db21b8636ef1623d85bae5be470ec2637972
ElroyKR <elroykr@gmail.com>
2024-04-15 16:42:42 +0900

    Exposed 세팅

--- src/main/kotlin/gg/kya/KyaApplication.kt
+++ src/main/kotlin/gg/kya/KyaApplication.kt
@@ -1,11 +1,14 @@
 package gg.kya
 
+import org.jetbrains.exposed.spring.autoconfigure.ExposedAutoConfiguration
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ImportAutoConfiguration(ExposedAutoConfiguration::class)
 class KyaApplication
 
 fun main(args: Array<String>) {
-	runApplication<KyaApplication>(*args)
+    runApplication<KyaApplication>(*args)
 }
